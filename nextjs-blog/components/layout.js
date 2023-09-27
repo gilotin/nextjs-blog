084b1a640f5f8f4343e9b1795f58f0fdfdbcbf23
Nikolay <103648794+gilotin@users.noreply.github.com>
2023-09-27 13:26:47 +0300

    changes to the website name

--- nextjs-blog/components/layout.js
+++ nextjs-blog/components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
 const name = "Nikolay Toshev";
-export const siteTitle = "Next.js Sample Website";
+export const siteTitle = "Nikolay Toshev blog-test";
 
 export default function Layout({ children, home }) {
     return (
